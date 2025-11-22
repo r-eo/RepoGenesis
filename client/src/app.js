614817f3eadf5b5eb243a056ebc334354da80b6b
r-eo <bipingoswami020@gmail.com>
2025-11-22 12:30:14 +0530

    67

--- client/src/app.js
+++ client/src/app.js
@@ -1,3 +1,28 @@
+import React, { useState, useEffect } from 'react';
+import { registerUser, loginUser, logSleep, getStats, getFriends, startSleep, endSleep } from './sleepApi';
+import './index.css';
+
+// Custom Clock Component
+const Clock = () => {
+    const [time, setTime] = useState(new Date());
+
+    useEffect(() => {
+        const timer = setInterval(() => setTime(new Date()), 1000);
+        return () => clearInterval(timer);
+    }, []);
+
+    return (
+        <span>
+            {time.toLocaleTimeString('en-US', {
+                hour12: false,
+                hour: '2-digit',
+                minute: '2-digit',
+                second: '2-digit'
+            })}
+        </span>
+    );
+};
+
 // Helper for Pomodoro Countdown
 const CountdownTimer = ({ minutes = 25, onComplete }) => {
     const [timeLeft, setTimeLeft] = useState(minutes * 60);
